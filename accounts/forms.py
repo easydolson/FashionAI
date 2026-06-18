9d12a68c1bed1e9650bd2649d9bba044444f10b6
Дарья Иванова <easydolson@gmail.com>
2026-06-18 15:41:15 +0700

    профиль

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Меняем метки на русские
+        self.fields['username'].label = 'Логин'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
