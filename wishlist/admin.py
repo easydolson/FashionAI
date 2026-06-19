2af1b1cf9c3416541e7b7101852929e599c36427
Дарья Иванова <easydolson@gmail.com>
2026-06-19 12:04:44 +0700

    избранное

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import WishlistItem
 
-# Register your models here.
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'look', 'added_at')
+    list_filter = ('user', 'added_at')
+    search_fields = ('user__username', 'product__name')
